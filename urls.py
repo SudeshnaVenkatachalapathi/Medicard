691ec705c64c24b64b9ed79871021a2f2be720bb
SudeshnaVenkatachalapathi <71323212+SudeshnaVenkatachalapathi@users.noreply.github.com>
2021-07-14 10:41:24 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,33 +1,13 @@
-"""medicard URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import include
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
 from . import views
-from django.conf import settings
 
-
+app_name = 'auth'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('siteauth.urls')),
-    path('', views.indexView, name="home"),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('reports/', include('user_acc.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('usernamecheck/', views.username_check, name='username_validate'),
+    path('activate/<slug:uidb64>/<slug:token>', views.acc_activate, name='activate'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- views.py
+++ views.py
@@ -1,39 +1,74 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.forms.forms import Form
+from user_acc.models import App_User
+from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
-from user_acc import models
-from django.views import View
-from django.utils.decorators import method_decorator
-from user_acc.forms import PatientProfile
-from django.views.generic import UpdateView
+from .forms import SignUpForm
+from django.contrib.sites.shortcuts import get_current_site
+from .tokens import account_activation_token
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.core.mail import EmailMessage
+from django.contrib import messages
+from django.contrib.auth import login
 
 
-def indexView(request):
-    return render(request, 'index/index.html')
+def signup(request):
+    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            mail_subject = 'User account activation'
+            message = render_to_string('registration/acc_active_email.html', {
+                'user': user,
+                'domain': get_current_site(request).domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user)
+            })
+            to_email = form.cleaned_data.get('email')
+            print(urlsafe_base64_encode(force_bytes(user.pk)))
+            print(account_activation_token.make_token(user))
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            messages.add_message(request, messages.SUCCESS, "Registration Successful, please verify your mail befor login")
+            return redirect(reverse_lazy('home'))
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileView(UpdateView):
-    def get(self, request):
-        form = PatientProfile()
-        user = models.App_User.objects.filter(user=request.user).values()[0]
-        if len(user):
-            ctx = {'user_data': user, 'form': form}
-            return render(request, 'index/profile.html', context=ctx)
-        ctx = {'form': form}
-        return render(request, 'index/profile.html', context=ctx)
+def username_check(request):
+    print(request.GET)
+    username = request.GET.get('username')
+    data = {
+        'is_taken': User.objects.filter(username__iexact=username).exists()
+    }
+    if data['is_taken']:
+        data['error'] = 'Username not available'
+    return JsonResponse(data)
 
-    def post(self, request):
-        user = User.objects.filter(id=request.user.id)[0]
-        data = dict(request.POST)
-        for k,v in data.items():
-            data[k] = v[0]
-        # user.delete()
-        # form = PatientProfile(request.POST)
-        # if form.is_valid():
-        #     form.save()
-        data['user'] = user
-        models.App_User.objects.update_or_create(user=user, defaults=data)
-        return redirect('/profile/')
-
+def acc_activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+        if user.is_active:
+            messages.add_message(request, messages.SUCCESS, 'Account has been verified already.')
+            return redirect(reverse_lazy('home'))
+    except:
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        patientuser = User.objects.filter(username=request.user)[0]
+        print(patientuser)
+        patient = App_User(user=patientuser)
+        patient.save()
+        messages.add_message(request, messages.SUCCESS, 'Account verified successfully')
+        return redirect(reverse_lazy('home'))
+    else:
+        messages.add_message(request, messages.ERROR, 'Account doesnot exist, Signup again')
